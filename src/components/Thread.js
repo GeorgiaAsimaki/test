f0ed5c1ae80a659ea2f259eb944d189556046e2c
Lungu Marius <Marius.Lungu@solera.com>
2022-12-02 12:07:28 +0100

    handle title

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -14,6 +14,7 @@ import Select from '@mui/material/Select';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import APIService from '../APIService';
 
 
 import {Routes, Route, useNavigate} from 'react-router-dom';
@@ -50,6 +51,14 @@ const Thread = ({title,text}) => {
     const onImageChange = (e) => {
         setImages([...e.target.files]);
     }
+    const handleTitle = async (e) => {
+      setPostTitle(e.target.value);
+      if(postTitle!=''){
+        const checked = await APIService.post.postTitle(postTitle);
+        console.log(checked);
+      }
+      
+    }
 
 
     const navigate = useNavigate();
@@ -60,9 +69,7 @@ const Thread = ({title,text}) => {
         //navigate('/passenger2');
     }
 
-   const handlePostTitle = (e) => {
-    setPostTitle(e.target.value);
-   }
+  
 
    const addTitle = () => {
     setTitleAdded(true);
@@ -94,11 +101,11 @@ const Thread = ({title,text}) => {
                 <Box sx={{ width: '100%' }}>
                     <Box sx={{ borderTop:2, borderBottom: 2, borderColor: 'divider', bgcolor:'#FAF0F2' }}>
                         <FormControl sx={{m: 3, top:5,  minWidth: 120 }} >
-                             <TextField id="title" label="Title" variant="outlined" onChange={handlePostTitle}/>
+                             <TextField id="title" label="Title" variant="outlined" onChange={handleTitle}/>
                              <FormHelperText>Please define the title of the post.</FormHelperText>
                         </FormControl>
                         <FormControl sx={{m: 3, top:13,  minWidth: 120 }} >
-                            <Button id="addTitle" variant="contained" onClick={addTitle} >
+                            <Button id="addTitle" variant="contained" onKeyDown={addTitle} >
                                 Create POST.
                             </Button>
                         </FormControl>
